8417ea64fd32f5d30f574ddfa96d5305fd62be6c
AnxoAV <anxoabuelo@gmail.com>
2024-03-10 22:08:15 +0100

    Ubicación de monedas y enemigos lvl 3

--- game.py
+++ game.py
@@ -1753,7 +1753,7 @@ def play(window, partida, volume):
         mercader = Mercader(2700-distance, 625, 100, 100) 
         firstBoss = Boss(8500-distance,450,100,100)
         checkpoint = Checkpoint(7700-distance,375,50,50, checkpoint_activated)
-        checkpoint_end = CheckpointEnd(9500-distance,550,50,50)
+        checkpoint_end = CheckpointEnd(8950-distance,575,50,50)
 
         all_enemies_group.add(meleeEnemie1)
         all_enemies_group.add(meleeEnemie2)
@@ -1767,7 +1767,7 @@ def play(window, partida, volume):
         meleeEnemies_group.add(meleeEnemie2)
         meleeEnemies_group.add(meleeEnemie3)
 
-    elif partida.level == 2 or partida.level == 3:
+    elif partida.level == 2:
         background,bg_image, heart_image, coin_image, gem_image = resource_manager.get_background("Cave2.png")
 
         ########## CONSTRUIR ENEMIGOS PARA NIVEL 2 (AHORA MISMO ESTA COMO NIVEL 1)#############
@@ -1783,9 +1783,8 @@ def play(window, partida, volume):
         meleeEnemie2 = MeleeEnemie(4375-distance,525,100,100,"HalflingRogue")
         mercader = Mercader(2700-distance, 655, 100, 100) 
         firstBoss = SecondBoss(8500-distance,480,100,100)
-        thirdBoss = ThirdBoss(9000-distance,390,100,100)
         checkpoint = Checkpoint(7700-distance,480,50,50, checkpoint_activated)
-        checkpoint_end = CheckpointEnd(9000-distance,575,50,50)
+        checkpoint_end = CheckpointEnd(9000-distance,610,50,50)
         meleeEnemie4 = Skull(5950 - distance,350,1000,1000,"Skull")
         meleeEnemie5 = Skull(6050 - distance,350,1000,1000,"Skull")
 
@@ -1796,14 +1795,42 @@ def play(window, partida, volume):
         all_enemies_group.add(rangedenemie1)
         all_enemies_group.add(rangedenemie2)
         all_enemies_group.add(firstBoss)
-        all_enemies_group.add(thirdBoss)
+  
         meleeEnemies_group.add(meleeEnemie1)
         meleeEnemies_group.add(meleeEnemie2)
         meleeEnemies_group.add(meleeEnemie4)
         meleeEnemies_group.add(meleeEnemie5)
         
-        
+    elif partida.level == 3:
+        background,bg_image, heart_image, coin_image, gem_image = resource_manager.get_background("Cave2.png")
 
+        if partida.checkpoint == 1:
+            distance = 8800
+            checkpoint_activated = True
+        player = Player(400,400,50,50, lives,fireball_group,partida.coins,partida.gems)
+        rangedenemie1 = RangedEnemies(1200-distance,270,100,100,30,arrow_group,"GnomeTinkerer")
+        meleeEnemie1 = Skull(2875 - distance,350,1000,1000,"Skull")
+        meleeEnemie2 = Skull(3525 - distance,350,1000,1000,"Skull")
+        meleeEnemie3 = MeleeEnemie(4475-distance,500,100,100,"Skeleton")
+        mercader = Mercader(3915-distance, 400, 100, 100)
+        firstBoss = ThirdBoss(9800-distance,390,100,100)
+        checkpoint = Checkpoint(9185-distance,290,50,50, checkpoint_activated)
+        checkpoint_end = CheckpointEnd(10400-distance,610,50,50)
+
+        all_enemies_group.add(meleeEnemie1)
+        all_enemies_group.add(meleeEnemie2)
+        all_enemies_group.add(meleeEnemie3)
+
+
+        all_enemies_group.add(rangedenemie1)
+
+        all_enemies_group.add(firstBoss)
+
+        meleeEnemies_group.add(meleeEnemie1)
+        meleeEnemies_group.add(meleeEnemie2)
+        meleeEnemies_group.add(meleeEnemie3)
+        
+        
     block_size = 96
     block2_size = 64 
     block3_size = 64 
@@ -1871,37 +1898,32 @@ def play(window, partida, volume):
         plat21 = [Platform(i*block_size + 5050-distance,HEIGHT - block_size - 425, plat_size)for i in range(0,1)]
 
         objects = [*floor,*floor2,*floor3,*column,*column2,*column3,*column4,*column5,*column6,*column7,*column8,*column9,*column10 ,*plat1,*plat2,*plat3,*plat4,*plat5,*plat6,*plat7,*plat8,*plat9,*plat10,*plat11,*plat12,*plat13,*plat14,*plat15,*plat16,*plat17,*plat18,*plat19,*plat20,*plat21]
-
-        gem1 = Gem(-950,650,40)
-        gem2 = Gem(-1000,650,40)
-        gem3 = Gem(425,25,40)
-        gem4 = Gem(6630,650,40)
         
         gems = []
         
-        putGems(-1000,650,1,gems)
-        putGems(425,25,1,gems)
-        putGems(6630,650,1,gems)
+        putGems(-1000 - distance,650,2,gems)
+        putGems(425 - distance,25,1,gems)
+        putGems(6630 - distance,650,1,gems)
 
         gems = pygame.sprite.Group(gems)
 
         coins = []
 
-        putCoins(1115,200,1,coins)
-        putCoins(920,200,1,coins)
-        putCoins(725,200,1,coins)
-        putCoins(1325,350,3,coins)
-        putCoins(2075,500,3,coins)
-        putCoins(4225,250,3,coins)
-        putCoins(4730,400,3,coins)
-        putCoins(2525,500,1,coins)
-        putCoins(2850,400,1,coins)
-        putCoins(2625,200,1,coins)
-        putCoins(2950,100,1,coins)
-        putCoins(5335,650,3,coins)
-        putCoins(5720,450,3,coins)
-        putCoins(6485,50,3,coins)
-        putCoins(6835,250,3,coins)
+        putCoins(1115 - distance,200,1,coins)
+        putCoins(920 - distance,200,1,coins)
+        putCoins(725 - distance,200,1,coins)
+        putCoins(1325 - distance,350,3,coins)
+        putCoins(2075 - distance,500,3,coins)
+        putCoins(4225 - distance,250,3,coins)
+        putCoins(4730 - distance,400,3,coins)
+        putCoins(2525 - distance,500,1,coins)
+        putCoins(2850 - distance,400,1,coins)
+        putCoins(2625 - distance,200,1,coins)
+        putCoins(2950 - distance,100,1,coins)
+        putCoins(5335 - distance,650,3,coins)
+        putCoins(5720 - distance,450,3,coins)
+        putCoins(6485 - distance,50,3,coins)
+        putCoins(6835 - distance,250,3,coins)
 
         coins = pygame.sprite.Group(coins)
  
@@ -1986,21 +2008,21 @@ def play(window, partida, volume):
 
         gems = []
 
-        putGems(2100,682,1,gems)
-        putGems(2370,106,1,gems)
+        putGems(2100 - distance,682,1,gems)
+        putGems(2370 - distance,106,1,gems)
 
         gems = pygame.sprite.Group(gems)
 
 
         coins = []
 
-        putCoins(1450,487,4,coins)
-        putCoins(2050,106,5,coins)
-        putCoins(3050,683,8,coins)
-        putCoins(3975,42,4,coins)
-        putCoins(3975,234,5,coins)
-        putCoins(3975,490,6,coins)
-        putCoins(6850,363,15,coins)
+        putCoins(1450 - distance,487,4,coins)
+        putCoins(2050 - distance,106,5,coins)
+        putCoins(3050 - distance,683,8,coins)
+        putCoins(3975 - distance,42,4,coins)
+        putCoins(3975 - distance,234,5,coins)
+        putCoins(3975 - distance,490,6,coins)
+        putCoins(6850 - distance,363,15,coins)
 
         coins = pygame.sprite.Group(coins)
 
@@ -2068,21 +2090,20 @@ def play(window, partida, volume):
 
          gems = []
 
-         putGems(2100,682,1,gems)
-         putGems(2370,106,1,gems)
+         putGems(2097 - distance,584,1,gems)
+         putGems(6790 - distance,83,2,gems)
 
          gems = pygame.sprite.Group(gems)
 
 
          coins = []
 
-         putCoins(1450,487,4,coins)
-         putCoins(2050,106,5,coins)
-         putCoins(3050,683,8,coins)
-         putCoins(3975,42,4,coins)
-         putCoins(3975,234,5,coins)
-         putCoins(3975,490,6,coins)
-         putCoins(6850,363,15,coins)
+         putCoins(1400 - distance,487,4,coins)
+         putCoins(0 - distance,533,2,coins)
+         putCoins(300 - distance,234,2,coins)
+         putCoins(700 - distance,42,3,coins)
+         putCoins(4449 - distance,282,2,coins)
+         putCoins(4721 - distance,133,2,coins)
 
          coins = pygame.sprite.Group(coins)   
 
@@ -2127,9 +2148,6 @@ def play(window, partida, volume):
                     
         player.loop(delta_time, all_enemies_group, window, partida, volume)
 
-        # print("PLAYER X = ",player.rect.x)
-        # print("PLAYER Y = ",player.rect.y)
-        
         for enemy in all_enemies_group:
             enemy.loop(player,volume)
             
